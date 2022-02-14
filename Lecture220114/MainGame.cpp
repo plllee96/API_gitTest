eee0100123e452c8d9d46ec253f6b9a70abc9022
jae <pkj8595@gmail.com>
2022-02-14 14:40:42 +0900

    pkj

--- Lecture220114/MainGame.cpp
+++ Lecture220114/MainGame.cpp
@@ -41,6 +41,7 @@ void MainGame::render() {
 	TextOut(getMemDC(), 450, 200, "오재훈 git", strlen("오재훈 git"));
 	TextOut(getMemDC(), 400, 250, "이형우 git", strlen("이형우 git"));
 	TextOut(getMemDC(), 350, 300, "김가연 git", strlen("김가연 git"));
+	TextOut(getMemDC(), 300, 250, "박경재 git", strlen("박경재 git"));
 	
 
 
