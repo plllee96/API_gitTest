5d034a10d53b859ae82a7d3b0b11815f3e01eb98
jae <pkj8595@gmail.com>
2022-02-14 13:20:10 +0900

    commit pkj

--- Lecture220114/MainGame.cpp
+++ Lecture220114/MainGame.cpp
@@ -22,7 +22,8 @@ void MainGame::render() {
 
 	SetTextColor(getMemDC(), RGB(0, 0, 0));
 	
-	TextOut(getMemDC(), 50, 50, "LHW_GitTest", strlen("LHW_GitTest"));
+	TextOut(getMemDC(), 50, 50, "LHW_GitTest", strlen("LHW_GitTest")); 
+	TextOut(getMemDC(), 100, 100, "박경재 git", strlen("박경재 git"));
 
 	TIMEMANAGER->render(getMemDC());
 	
