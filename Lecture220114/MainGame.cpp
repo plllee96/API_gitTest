a86d505387b6c4cef4110061ba123af6c0289f5f
plllee96 <plllee96@naver.com>
2022-02-14 13:50:34 +0900

    LHW commit_2

--- Lecture220114/MainGame.cpp
+++ Lecture220114/MainGame.cpp
@@ -26,6 +26,7 @@ void MainGame::render() {
 	TextOut(getMemDC(), 100, 100, "박경재 git", strlen("박경재 git"));
 	TextOut(getMemDC(), 150, 150, "박경재 git", strlen("박경재 git"));
 	TextOut(getMemDC(), 200, 200, "오재훈 git", strlen("오재훈 git"));
+	TextOut(getMemDC(), 250, 250, "이형우 git", strlen("이형우 git"));
 
 	TIMEMANAGER->render(getMemDC());
 	
