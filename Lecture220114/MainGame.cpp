59a7f21ccf603e62257206b48bd0143d16eca7aa
plllee96 <plllee96@naver.com>
2022-02-14 15:14:58 +0900

    do not use

--- Lecture220114/MainGame.cpp
+++ Lecture220114/MainGame.cpp
@@ -41,7 +41,6 @@ void MainGame::render() {
 	TextOut(getMemDC(), 400, 250, "이형우 git", strlen("이형우 git"));
 	TextOut(getMemDC(), 350, 300, "김가연 git", strlen("김가연 git"));
 	TextOut(getMemDC(), 300, 250, "박경재 git", strlen("박경재 git"));
-	
 	TextOut(getMemDC(), 300, 350, "박경재 git", strlen("박경재 git"));
 	TextOut(getMemDC(), 250, 400, "오재훈 git", strlen("오재훈 git"));
 	TextOut(getMemDC(), 200, 450, "이형우 git", strlen("이형우 git"));
